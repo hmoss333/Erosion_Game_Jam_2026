6e9a8fd536ad24c6577f3e9cf0fbb15d05c1094d
Henry Moss <hdamoss@gmail.com>
2026-02-26 11:48:29 -0500

    Started creating a door script that will let the player open doors when they are set to active; created a new doorway prefab, adding a doorpivot object and adding the door script to the base object; Created a set of new scripts for all future interact objects; created folders to distinguish all interact objects

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : InteractObject
+{
+    [SerializeField] GameObject doorRotation;
+    [SerializeField] float openRotation = 170.0f;
+    bool openDoor;
+
+
+    private void Update()
+    {
+        if (openDoor)
+        {
+            doorRotation.transform.Rotate(0.0f, openRotation * Time.deltaTime, 0.0f, Space.Self);
+
+            if (Mathf.Abs(doorRotation.transform.localRotation.eulerAngles.y) > Mathf.Abs(openRotation))
+                openDoor = false;
+        }
+    }
+
+    public override void Interact()
+    {
+        if (active)
+        {
+            base.Interact();
+            Activate();
+            openDoor = true;
+            BoxCollider collider = GetComponent<BoxCollider>();
+            collider.enabled = false;
+        }
+    }
+}
