3bfa6556ed1113e1849775b008a54e5317c20ec6
Henry Moss <hdamoss@gmail.com>
2026-02-26 23:20:26 -0500

    Added more material packs, deleted repeated materials with unused extensions; updated the gameplay controller class to allow for all environmental objects to have their materials updated when progressing to the next cycle; updated environmental prefabs to use correct layers; imported environmental objects

--- Assets/Scripts/Controllers/GameplayController.cs
+++ Assets/Scripts/Controllers/GameplayController.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using System.Linq;
 
 public class GameplayController : MonoBehaviour
 {
     public static GameplayController instance;
-
-    public int cycleNum { get; private set; }
+    [SerializeField] GameObject[] environmentalObjs;
+    [SerializeField] Material[] environmentalMats;
+    public int cycleNum { get; private set; }   
     [Header("Final Cycle Triggers")]
     public UnityEvent m_OnTrigger = new UnityEvent();
 
@@ -19,15 +21,41 @@ public class GameplayController : MonoBehaviour
         else
             instance = this;
 
+        environmentalObjs = FindGameObjectsInLayer(7);
         cycleNum = 0;
         FadeController.instance.StartFade(0.0f, 3f);
+    }
+
+    GameObject[] FindGameObjectsInLayer(int layer)
+    {
+        var goArray = FindObjectsOfType(typeof(GameObject)) as GameObject[];
+        var goList = new List<GameObject>();
+        for (int i = 0; i < goArray.Length; i++)
+        {
+            if (goArray[i].layer == layer)
+            {
+                goList.Add(goArray[i]);
+            }
+        }
+        if (goList.Count == 0)
+        {
+            return null;
+        }
+        return goList.ToArray();
     }
 
     public void IncrementCycle()
     {
         cycleNum++;
 
-        if (cycleNum > 3)
+        foreach(GameObject envObj in environmentalObjs)
+        {
+            List<Material> mats = envObj.GetComponent<Renderer>().materials.ToList();
+            mats[0] = environmentalMats[cycleNum - 1];
+            envObj.GetComponent<Renderer>().materials = mats.ToArray();
+        }
+
+        if (cycleNum >= 3)
         {
             m_OnTrigger.Invoke();
         }
