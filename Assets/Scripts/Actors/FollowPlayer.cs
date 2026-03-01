6b20c2fbe7971eb70f6704766860e49f1fe7bf76
Henry Moss <hdamoss@gmail.com>
2026-03-01 16:33:00 -0500

    ???

---
+++ Assets/Scripts/Actors/FollowPlayer.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(PlayerController.instance.transform);
+    }
+}
