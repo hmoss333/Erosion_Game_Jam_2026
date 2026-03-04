678e89015cd5e779c6a03a7d59973fbc7c56e4c9
Henry Moss <hdamoss@gmail.com>
2026-03-04 00:13:59 -0600

    Adjusted code-locked room size; locked the final hallway door so you cannot escape into the void

--- Assets/Scripts/Actors/FollowPlayer.cs
+++ Assets/Scripts/Actors/FollowPlayer.cs
@@ -4,9 +4,24 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
+    [SerializeField] float speed;
+
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(PlayerController.instance.transform);
+        // Determine which direction to rotate towards
+        Vector3 targetDirection = PlayerController.instance.transform.position - transform.position;
+
+        // The step size is equal to speed times frame time.
+        float singleStep = speed * Time.deltaTime;
+
+        // Rotate the forward vector towards the target direction by one step
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+
+        // Draw a ray pointing at our target in
+        Debug.DrawRay(transform.position, newDirection, Color.red);
+
+        // Calculate a rotation a step closer to the target and applies rotation to this object
+        transform.rotation = Quaternion.LookRotation(newDirection);
     }
 }
